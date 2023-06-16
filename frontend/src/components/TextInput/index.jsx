ba2f8ffd5ae3c37c42b022a5bc0bb25f07d1edef
unknown <mirvatbarakat1@gmail.com>
2023-06-17 00:19:12 +0300

    frontend<> Added text input component structure

---
+++ frontend/src/components/TextInput/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const TextInput = ({ label, value, onChange }) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <input type="text" value={value} onChange={onChange} />
+      </div>
+    );
+  };
+
+export default TextInput;
