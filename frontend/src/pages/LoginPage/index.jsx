86ab72eae3b8965b5b82b75b680ab12429f934f7
unknown <mirvatbarakat1@gmail.com>
2023-06-17 16:28:44 +0300

    frontend<> Added styles to landing page

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -6,7 +6,9 @@ const LandingPage = () => {
   return (
     <div className="main-container">
       <div className="system-name-container">
-        <h1>System Name</h1>
+        <h1 className="system-name">My Time Off</h1>
+        <p className="welcome-message">Welcome to your vacation hub!</p>
+        <p className="intro-message">Plan your well-deserved time off, keep track of your vacation days, and make the most out of your breaks. Our system simplifies the process, ensuring a seamless experience for managing your vacations. Get started today and embark on memorable adventures!</p>
       </div>
       <div className="form-container">
         <LoginRegisterForm />
