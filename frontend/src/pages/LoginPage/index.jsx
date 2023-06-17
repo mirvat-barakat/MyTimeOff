976456f9ed08fb53614d53f5a362a9db07f6c6e3
unknown <mirvatbarakat1@gmail.com>
2023-06-17 16:47:51 +0300

    frontend<> Added img to landing page and adjusted styling

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -5,10 +5,13 @@ import LoginRegisterForm from '../../components/LoginRegisterForm';
 const LandingPage = () => {
   return (
     <div className="main-container">
+      <div className='welcome'>
       <div className="system-name-container">
         <h1 className="system-name">My Time Off</h1>
         <p className="welcome-message">Welcome to your vacation hub!</p>
         <p className="intro-message">Plan your well-deserved time off, keep track of your vacation days, and make the most out of your breaks. Our system simplifies the process, ensuring a seamless experience for managing your vacations. Get started today and embark on memorable adventures!</p>
+      </div>
+      <img src={"vacation.jpg"} alt="Vacation" className='img'/>
       </div>
       <div className="form-container">
         <LoginRegisterForm />
