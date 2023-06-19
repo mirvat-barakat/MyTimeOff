0ee1c0b02ce0a8737e7155b624f3d973dce75c69
unknown <mirvatbarakat1@gmail.com>
2023-06-19 11:14:02 +0300

    frontend<> Updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import LandingPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -7,7 +8,8 @@ const App = () => {
   return (
           <Router>
             <Routes>
-              <Route path="/" element={<LandingPage />} />  
+              <Route path="/" element={<LandingPage />} /> 
+              <Route path="/home" element={<HomePage />} /> 
             </Routes>
           </Router>
   );
