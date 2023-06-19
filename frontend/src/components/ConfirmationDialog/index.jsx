fe252786ce94b34bd7f51047c4984cdea24c7bed
unknown <mirvatbarakat1@gmail.com>
2023-06-19 11:37:40 +0300

    frontend<> Added confirmation catalog component

---
+++ frontend/src/components/ConfirmationDialog/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const Confirmation = (props) => {
+
+    const { message, onCancel, onConfirm } = props;
+
+    return(
+        <div className="confirmation-dialog">
+            <div className="confirmation-dialog-message">{message}</div>
+            <div className="confirmation-dialog-buttons">
+                <button className="confirmation-dialog-button  cancel"  onClick={onCancel}>Cancel</button>
+                <button className="confirmation-dialog-button  confirm"  onClick={onConfirm}>Confirm</button>
+            </div>
+        </div>
+    );
+};
+export default Confirmation;
