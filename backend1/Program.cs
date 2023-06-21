6292037a3c676c84749cda8f15fd596cf4626d6d
unknown <mirvatbarakat1@gmail.com>
2023-06-21 21:19:15 +0300

    backend<> Solved CORS error

--- backend1/Program.cs
+++ backend1/Program.cs
@@ -9,29 +9,35 @@ using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add configuration
+
 builder.Configuration.AddJsonFile("appsettings.json");
 
 var connectionString = "Server=127.0.0.1;Port=3306;Database=vacation_db;Uid=root;Pwd=;";
 
-// Register services
+
 builder.Services.AddDbContext<VacationDbContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 26))));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-// Build the host
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<VacationDbContext>();
-    // Perform any database initialization or seeding here
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
 
 app.MapControllers();
 
